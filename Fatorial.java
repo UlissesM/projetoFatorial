720a83892966b73d2b6c54337b2a1f261d22ee3a
Ulisses <ulissesmaia1311@gmail.com>
2017-11-18 20:29:22 -0200

    Alteração para lancar execao em casa de numeros negativos

--- Fatorial.java
+++ Fatorial.java
@@ -1,7 +1,9 @@
 public class Fatorial {
 	// VERSAO ITERATIVA DO CALCULO DO FATORIAL	
-	public int calculaFatorial(int n){
+	public int calculaFatorial(int n) throws IllegalArgumentException{
 		int resultado=1;
+		if(n<0)
+			throw new IllegalArgumentException("não existe fatorial para numeros negativos");
 		for(int i = 2; i<=n;i++){
 			resultado*=i;
 		}
